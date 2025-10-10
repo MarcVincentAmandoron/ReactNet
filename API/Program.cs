8e76c21dd0587722ea949394bda65316d63b2051
MarcVincentAmandoron <marc.amandoron@gmail.com>
2025-10-09 22:28:20 -0700

    Created a walking skeleton

---
+++ API/Program.cs
@@ -0,0 +1,42 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+using Persistence;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddDbContext<AppDbContext>(opt =>
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.MapControllers();
+
+// Use service locator pattern
+// Creating a services scope w/ using keyword
+// --> When this function goes out of scope AKA the app is run anything that is used in this scope
+// --> will be disposed of by the framework so we do not need to clean up after ourselvs
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+
+try
+{
+    // get access to our database context and use that context to query the database
+    var context = services.GetRequiredService<AppDbContext>();
+    // aync applies any migrations for the context to the database
+    await context.Database.MigrateAsync();
+    await DbInitializaer.SeedData(context);
+}
+catch (Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error occurred during migration.");
+}
+
+app.Run();
