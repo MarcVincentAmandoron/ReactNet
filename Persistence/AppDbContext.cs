b9b2d4ec4cef4307909c221d40b6c765ee01996f
MarcVincentAmandoron <marc.amandoron@gmail.com>
2025-10-09 22:32:52 -0700

    configured git ignore

--- Persistence/AppDbContext.cs
+++ Persistence/AppDbContext.cs
@@ -1,4 +1,5 @@
 using System;
+using Domain;
 using System.Diagnostics;
 using Microsoft.EntityFrameworkCore;
 
@@ -6,5 +7,5 @@ namespace Persistence;
 
 public class AppDbContext(DbContextOptions options) : DbContext(options)
 {
-    public required DbSet<Activity> Activities { get; set; }
+    public required DbSet<Domain.Activity> Activities { get; set; }
 }
---
+++ Persistence/Migrations/20251001063201_ChangedProps.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangedProps : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "isCancelled",
+                table: "Activities",
+                type: "INTEGER",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "INTEGER");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Longitude",
+                table: "Activities",
+                type: "REAL",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "REAL");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Latitude",
+                table: "Activities",
+                type: "REAL",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "REAL");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Date",
+                table: "Activities",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "isCancelled",
+                table: "Activities",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "INTEGER",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Longitude",
+                table: "Activities",
+                type: "REAL",
+                nullable: false,
+                defaultValue: 0.0,
+                oldClrType: typeof(double),
+                oldType: "REAL",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Latitude",
+                table: "Activities",
+                type: "REAL",
+                nullable: false,
+                defaultValue: 0.0,
+                oldClrType: typeof(double),
+                oldType: "REAL",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Date",
+                table: "Activities",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT");
+        }
+    }
+}
